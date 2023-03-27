d5e06d92f3a5b8e3d1e13e626097c2d75ac7ea30
lumanya <shaibulumanya@gmail.com>
2023-03-27 11:48:09 +0300

    puppet manifest to manage file resource

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet Mannifest that manage file resource
+
+file {'/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
