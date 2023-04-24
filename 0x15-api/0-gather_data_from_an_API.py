513c31b55f27555b4125e9538cc48ebce8622eda
lumanya <shaibulumanya@gmail.com>
2023-04-24 14:15:15 +0300

    fix pycode style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,25 +9,25 @@ import requests
 
 if __name__ == "__main__":
     """ request user name from API"""
-    task_done = 0
-    total_task = 0
+    employeeId = int(argv[1])
+    done = 0
+    tasks = 0
     task_title = {}
 
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     r = requests.get(user_url)
-    user_data = r.json()
+    employee = r.json().get('name')
 
     # Gettig todos list
     todo_url = "https://jsonplaceholder.typicode.com/todos"
     r_todo = requests.get(todo_url)
     for todo in r_todo.json():
-        if todo.get('userId') == int(argv[1]):
-            total_task += 1
-            if todo.get('completed') == True:
-                task_done += 1
+        if todo.get('userId') == employeeId:
+            tasks += 1
+            if todo.get('completed'):
+                done += 1
 
-    print("Employee {} is done with tasks({}/{})".\
-          format(user_data.get('name'), task_done, total_task))
+    print(f"Employee {employee} is done with tasks({done}/{tasks})")
     for todo in r_todo.json():
-        if todo.get('userId') == int(argv[1]) and todo.get('completed') == True:
+        if todo.get('userId') == employeeId and todo.get('completed'):
             print("\t {}".format(todo.get('title')))
