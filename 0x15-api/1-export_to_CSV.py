44745a4925e08643f33002091e95bd28668817aa
lumanya <shaibulumanya@gmail.com>
2023-04-25 11:03:32 +0300

    write to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees and write to csv
+ format: "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS","TASK_TITLE"
+FILENAME: USER_ID.csv
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        task.pop('id', None)
+        task.update({'user_name': employeeName})
+
+    filename = "{}.csv".format(employeeId)
+    with open(filename, 'w') as file:
+        # Difine fields name
+        fieldnames = ['userId', 'user_name', 'completed', 'title']
+        writer = csv.DictWriter(file, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+
+        # Write the data to the file
+        for row in tasks:
+            writer.writerow(row)
