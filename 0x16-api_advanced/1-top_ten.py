ca5f34a954c8ce326a4402eda924f2c944a2433a
lumanya <shaibulumanya@gmail.com>
2023-06-14 14:12:35 +0300

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
